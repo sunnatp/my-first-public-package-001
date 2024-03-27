43d660d7c47d40bd2eaeb5c30fd5c9eadcce2cb7
sunnatp <xabibullayevsunnat9@gmail.com>
2024-03-27 16:18:11 +0500

    change packagehan

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-const reverseStr = (str) => {
-    let result = "";
-    for (const letter of str) {
-        result = letter + result
+// const reverseStr = (str) => {
+//     let result = "";
+//     for (const letter of str) {
+//         result = letter + result
+//     }
+
+//     return result
+// }
+
+// module.exports = reverseStr;
+
+
+
+const axios = require("axios");
+
+const getData = async() => {
+    try {
+        let res = await axios.get("https://jsonplaceholder.typicode.com/users")
+
+        return res;
+    } catch (error) {
+        console.log(error);
     }
-
-    return result
 }
-
-module.exports = reverseStr;
+module.exports = getData;
